77b26312d540dd6fc9d6f305eca130bf753e6b49
Yasnovskiy <andrey5.1997@mail.ru>
2021-11-28 23:32:57 +0500

    commit(): Исправления после ревью

--- src/main.js
+++ src/main.js
@@ -1,26 +1,29 @@
 import {createSiteMenuTemplate} from './view/site-menu-view.js';
 import {createSiteFilterTemplate} from './view/site-filter-view.js';
-import {createSiteCortTemplate} from './view/site-cort-view.js';
+import {createSiteSortTemplate} from './view/site-sort-view.js';
 import {createSiteAddNewTemplate} from './view/site-add-new-view.js';
-import {createSiteItemTemplate} from './view/site-item-view.js';
+import {createSiteEventTemplate} from './view/site-event-view.js';
+import {createSiteEventsListTemplate} from './view/site-events-list-view.js';
 
 import {renderTemplate, RenderPosition} from './render.js';
 
+const main = document.querySelector('main');
 const siteMainElement = document.querySelector('.trip-main');
 const siteMenuElement = siteMainElement.querySelector('.trip-controls__navigation');
 const siteFilterElement = siteMainElement.querySelector('.trip-controls__filters');
-
-const siteItemElement = document.querySelector('.trip-events__list');
 
 const TASK_COUNT = 3;
 
 renderTemplate(siteMenuElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteFilterElement, createSiteFilterTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(siteItemElement, createSiteCortTemplate(), RenderPosition.BEFOREBEGIN);
+renderTemplate(main, createSiteEventsListTemplate(), RenderPosition.BEFOREEND);
+
+const siteItemElement = main.querySelector('.trip-events__list');
+
+renderTemplate(siteItemElement, createSiteSortTemplate(), RenderPosition.BEFOREBEGIN);
 renderTemplate(siteItemElement, createSiteAddNewTemplate(), RenderPosition.AFTERBEGIN);
 
-
 for (let i = 0; i < TASK_COUNT; i++) {
-  renderTemplate(siteItemElement, createSiteItemTemplate(), RenderPosition.BEFOREEND);
+  renderTemplate(siteItemElement, createSiteEventTemplate(), RenderPosition.BEFOREEND);
 }
 
