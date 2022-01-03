06c7f72c0b66568b5f076f41da74727367223957
Yasnovskiy <andrey5.1997@mail.ru>
2022-01-03 19:42:03 +0500

    commit(3.1.5): Поместил прайс в инпут для правильного отображения

--- src/view/site-add-new-view.js
+++ src/view/site-add-new-view.js
@@ -84,9 +84,9 @@ const createSiteAddNewTripTemplate = (obj) => {
       <div class="event__field-group  event__field-group--price">
         <label class="event__label" for="event-price-1">
           <span class="visually-hidden">Price</span>
-          &euro; ${basePrice}
+          &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value=${basePrice}>
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
